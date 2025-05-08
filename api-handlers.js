cf5aa840a3bbba8787ce16ab7c3de19d766c2423
Chetan7528 <kaklotarchetan7528@gmail.com>
2025-05-08 15:37:43 +0530

    minor change

--- api-handlers.js
+++ api-handlers.js
@@ -1,25 +1,27 @@
-const { getExecOutput } = require( './api-handlers-helpers' );
+const { getExecOutput } = require('./api-handlers-helpers');
 var os = require('os');
 
-exports.testSpeedHandler = async() => {
-
-    const testCommandOutput = await getExecOutput( 'fast --upload --json' )
+exports.testSpeedHandler = async () => {
+    // try {
+    const testCommandOutput = await getExecOutput('fast --upload --json')
 
     //Handle no internet error
-    if ( testCommandOutput.data == '› Please check your internet connection\n\n\n' ) 
-    {    
+    if (testCommandOutput.data == '› Please check your internet connection\n\n\n') {
         testCommandOutput.status = 400
     }
 
-    if ( testCommandOutput.status !== 200 ) 
-    {    
+    if (testCommandOutput.status !== 200) {
         return testCommandOutput
     }
+    // } catch (err) {
+    //     return err
+    // }
+    console.log('Data before parsing:', testCommandOutput); // Add this line
 
     return {
         ...testCommandOutput,
         data: {
-            ...JSON.parse( testCommandOutput.data ),
+            ...JSON.parse(testCommandOutput.data),
             server: os.hostname(),
             os: process.platform,
         }
